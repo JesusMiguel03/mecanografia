a1461b03b806d436550a8e4624c9d77e44985445
JesusMiguel03 <106354441+JesusMiguel03@users.noreply.github.com>
2022-07-26 16:47:33 -0400

    Update code.js
    +cambios
    +comentarios
    +modo oscuro

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
 function codeInit() {
-    clean() // Llama a la función de limpiar pantalla
+    clean() // [Esp] Llama a la función de limpiar pantalla / [Eng] Calls clean screen function 
     const input = document.querySelector('.input')
     const contenedor = document.querySelector('.container')
     var win = false
 
-    // Crea nros aleatorios
+    // [Esp] Crea nros aleatorios / [Eng] Create random numbers
     function rNum(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
-    /* Crea elementos p y guarda las palabras en cada uno */
+    // [Esp] Crea elementos p y guarda las palabras en cada uno / [Eng] Generate <p> and keep every word
     function generateP() {
         let documentFrag = document.createDocumentFragment()
         for (var i = 0; i < eListEnd; i++) {
@@ -22,83 +22,83 @@ function codeInit() {
         }
     }
 
-    /* Compara la tecla presionada con el texto */
-    function keyboard(string) {
+    // [Esp] Compara la tecla presionada con el texto / [Eng] Compares key & word
+    function keyboard(elements_string) {
         var contador = 0
         var i = 0
-        var contenedorComp = ""
+        let temp = ""
 
-        // Empieza la comparación
-        input.addEventListener('keypress', (e)=> {
+        // [Esp] Empieza la comparación / [Eng] Begins the comparation
+        input.addEventListener('keypress', (e) => {
             let currentKey = e.key
-            contenedorComp += currentKey
-            if (currentKey == string[i]) {
+            temp += currentKey
+
+            if (currentKey == elements_string[i]) {
                 i++
                 contador++
             } else {
                 i++
             }
 
-            // Cuando termina la oración
-            
-            if (i ==tEnd) {
+            // [Esp] Cuando termina la oración / [Eng] When finish
+            if (i == tEnd) {
                 let generator = `<p id='alert' class='text stop'></p> <p id='score' class='text score'></p> <p id='time' class='text time'></p>`
                 contenedor.innerHTML = generator
                 win = true
 
-                // Mostrar resultados
+                // [Esp] Mostrar resultados / [Eng] Show results
                 function end() {
                     document.getElementById('textarea').remove()
                     let finalText = ""
                     document.getElementById('alert').textContent = '¡La prueba ha acabado!'
-                    document.getElementById('score').textContent = `Precisión: [${((contador*100)/tEnd).toFixed(2)}%] Letras: [${contador}/${tEnd}]`
+                    document.getElementById('score').textContent = `Precisión: [${((contador * 100) / tEnd).toFixed(2)}%] Letras: [${contador}/${tEnd}]`
                     document.getElementById('time').textContent = `Has tardado: ${seg}s en completar la prueba.`
                 }
                 end()
             }
         })
     }
-    /* Array con palabras, array vacio y cadena vacia */
+    // [Esp] Array con palabras, array vacio y cadena vacia / [Eng] Words and others variables
     var eList = []
-    var elements = ['el','elefante','camina','por','la','sabana','en','busca','de','alimento','la','pantera','caza','a','su','presa','con','sus','garras','comer','pescado','carne',
-                                'miel','hierbas','medicina','comida','huevos','casa','hogar','propiedad','consultorio','edificio','en','un','cocina','hombre','mujer','niño','castigo',
-                                'llanto','azúcar','café','agua','bebida','pez','pollo','bistec','ballena','belleza','construir','parada','abono','asfalto','cemento','tierra','madera','ella','ellos',
-                                'vosotros','nosotros','tu','caminar','correr','andar','respirar','nadar','océano','playa','palmera','divisa','economía','historia','física','química','ácido',
-                                'proteínas','cafeína','descubrimiento','hallazgo','pieda','troncos','mosquitos','mosca','asterisco','marioneta','persona','chico','chica','asiático','europeo',
-                                'francés','español','indio','americano','brazilero','dominicano','venezolano','colombiano','neozelandés','dólar','euro','petróleo','gas','cuerda','pradera']
-    var string = ""
+    var elements = ['el', 'elefante', 'camina', 'por', 'la', 'sabana', 'en', 'busca', 'de', 'alimento', 'la', 'pantera', 'caza', 'a', 'su', 'presa', 'con', 'sus', 'garras', 'comer', 'pescado', 'carne',
+        'miel', 'hierbas', 'medicina', 'comida', 'huevos', 'casa', 'hogar', 'propiedad', 'consultorio', 'edificio', 'en', 'un', 'cocina', 'hombre', 'mujer', 'niño', 'castigo',
+        'llanto', 'azucar', 'cafe', 'agua', 'bebida', 'pez', 'pollo', 'bistec', 'ballena', 'belleza', 'construir', 'parada', 'abono', 'asfalto', 'cemento', 'tierra', 'madera', 'ella', 'ellos',
+        'vosotros', 'nosotros', 'tu', 'caminar', 'correr', 'andar', 'respirar', 'nadar', 'oceano', 'playa', 'palmera', 'divisa', 'economia', 'historia', 'fisica', 'quimica', 'acido',
+        'proteinas', 'cafeina', 'descubrimiento', 'hallazgo', 'pieda', 'troncos', 'mosquitos', 'mosca', 'asterisco', 'marioneta', 'persona', 'chico', 'chica', 'asiatico', 'europeo',
+        'frances', 'español', 'indio', 'americano', 'brazilero', 'dominicano', 'venezolano', 'colombiano', 'neozelandes', 'dolar', 'euro', 'petroleo', 'gas', 'cuerda', 'pradera']
+    var elements_string = ""
 
-    /* Selecciona elementos para crear la oración, cambiar el 5 aumenta las palabras */
-    for (var i = 0; i < rNum(30, 80); i++) {
-        var random = rNum(0, elements.length-1)
+    // [Esp] Selecciona elementos para crear la oración / [Eng] Generate the sentence
+    for (var i = 0; i < rNum(1, 5); i++) {
+        var random = rNum(0, elements.length - 1)
         eList.push(elements[random])
     }
 
-    /* Cambia la ',' por un espacio y calcula la longitud*/
-    string = eList.join(' ')
-    var tEnd =  string.length
+    // [Esp] Cambia la ',' por un espacio y calcula la longitud / [Eng] Convert the array into a string
+    elements_string = eList.join(' ')
+    var tEnd = elements_string.length
     var eListEnd = eList.length
 
-    generateP() // Genera los <p></p> con cada palabra
-    keyboard(string) // Compara la tecla presionada con la letra de la palabra
+    generateP() // [Esp] Genera los <p></p> con cada palabra / [Eng] Calls the <p></p> function generator
+    keyboard(elements_string) // [Esp] Compara la tecla presionada con la letra de la palabra // [Eng] Comparation function
 
-    /* Contador */
+    // [Esp] Contador / [Eng] Counter
     var seg = 0
     var min = 1
     var seg2 = 0
-    input.addEventListener('click', (e)=> {
+    input.addEventListener('click', (e) => {
         const timer = document.querySelector('.timer')
-        const interval = setInterval(()=> { // Inicia Contador
-            if (min == 1) {
-                timer.innerHTML = `${min}:${seg2}0`
-                min = 0
-                seg2 = 60
-            }
-            timer.innerHTML = `${min}:${seg2}`
+        const interval = setInterval(() => { // [Esp] Inicia Contador / [Eng] Timer start
             seg++
             seg2--
+            if (min == 1) {
+                min--
+                seg2 = 60
+            }
             if (seg2 < 10) {
                 timer.innerHTML = `${min}:0${seg2}`
+            } else {
+                timer.innerHTML = `${min}:${seg2}`
             }
             if (seg == 60) {
                 min++
@@ -106,27 +106,33 @@ function codeInit() {
             }
             return seg, min
         }, 1000)
-        setTimeout(()=> { // Se detiene contador a los 60s
+        setTimeout(() => { // [Esp] Se detiene contador a los 60s / [Eng] Stops after 1 min
             clearInterval(interval)
-            if (win == false) {
-                let fail = `<p id="fail" class="text"></p>`
-                    contenedor.innerHTML = fail
-                    document.getElementById('fail').textContent =  '¡Has fallado la prueba!'
-                    document.getElementById('container').style.display = 'none'
-            }
         }, 61000)
     })
     return win
 }
 
-codeInit() // Inicia código
+codeInit() // [Esp] Inicia código / [Eng] Code begins
 
-// Limpia la pantalla
+// [Esp] Limpia la pantalla / [Eng] Clean screen
 function clean() {
     const container = document.querySelector('.container')
-    container.innerHTML = ''
     const containerText = document.getElementById('container')
+    container.innerHTML = ''
     containerText.style.display = 'block'
     let text = `<textarea id="textarea" class="input" autofocus></textarea>`
     containerText.innerHTML = text
+}
+
+function toggler() {
+    const change = document.body
+    const h2 = document.querySelector('.page-title')
+    const container = document.querySelector('.container')
+    const container2 = document.getElementById('container')
+    
+    change.classList.toggle("dark-mode")
+    h2.classList.toggle("page-title-dark-mode")
+    container.classList.toggle("container-dark-mode")
+    container2.classList.toggle("container-dark-mode")
 }
